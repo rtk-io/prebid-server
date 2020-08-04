ba6248f9919ebd6858a39344bfe679b5be8cfb2e
Bartosz Burclaf <burclaf@gmail.com>
2020-08-04 12:21:27 +0200

    add auction-only logging

---
+++ analytics/fs_auction/auction_module.go
@@ -0,0 +1,85 @@
+package filesystem
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	"github.com/chasex/glog"
+	"github.com/prebid/prebid-server/analytics"
+)
+
+type RequestType string
+
+const (
+	AUCTION RequestType = "/openrtb2/auction"
+)
+
+//Module that can perform transactional logging
+type FileLogger struct {
+	Logger *glog.Logger
+}
+
+//Writes AuctionObject to file
+func (f *FileLogger) LogAuctionObject(ao *analytics.AuctionObject) {
+	//Code to parse the object and log in a way required
+	var b bytes.Buffer
+	b.WriteString(jsonifyAuctionObject(ao))
+	f.Logger.Debug(b.String())
+	f.Logger.Flush()
+}
+
+//Writes VideoObject to file
+func (f *FileLogger) LogVideoObject(vo *analytics.VideoObject) {
+	// do nothing
+}
+
+//Logs SetUIDObject to file
+func (f *FileLogger) LogSetUIDObject(so *analytics.SetUIDObject) {
+	// do nothing
+}
+
+//Logs CookieSyncObject to file
+func (f *FileLogger) LogCookieSyncObject(cso *analytics.CookieSyncObject) {
+	// do nothing
+}
+
+//Logs AmpObject to file
+func (f *FileLogger) LogAmpObject(ao *analytics.AmpObject) {
+	// do nothing
+}
+
+//Method to initialize the analytic module
+func NewFileLogger(filename string) (analytics.PBSAnalyticsModule, error) {
+	options := glog.LogOptions{
+		File:  filename,
+		Flag:  glog.LstdNull,
+		Level: glog.Ldebug,
+	}
+	if logger, err := glog.New(options); err == nil {
+		return &FileLogger{
+			logger,
+		}, nil
+	} else {
+		return nil, err
+	}
+}
+
+type fileAuctionObject analytics.AuctionObject
+
+func jsonifyAuctionObject(ao *analytics.AuctionObject) string {
+	type alias analytics.AuctionObject
+	b, err := json.Marshal(&struct {
+		Type RequestType `json:"type"`
+		*alias
+	}{
+		Type:  AUCTION,
+		alias: (*alias)(ao),
+	})
+
+	if err == nil {
+		return string(b)
+	} else {
+		return fmt.Sprintf("Transactional Logs Error: Auction object badly formed %v", err)
+	}
+}
---
+++ analytics/fs_auction/auction_module_test.go
@@ -0,0 +1,35 @@
+package filesystem
+
+import (
+	"net/http"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/prebid/prebid-server/analytics"
+)
+
+const TEST_DIR string = "testFiles"
+
+func TestAuctionObject_ToJson(t *testing.T) {
+	ao := &analytics.AuctionObject{
+		Status: http.StatusOK,
+	}
+	if aoJson := jsonifyAuctionObject(ao); strings.Contains(aoJson, "Transactional Logs Error") {
+		t.Fatalf("AuctionObject failed to convert to json")
+	}
+}
+
+func TestFileLogger_LogObjects(t *testing.T) {
+	if _, err := os.Stat(TEST_DIR); os.IsNotExist(err) {
+		if err = os.MkdirAll(TEST_DIR, 0755); err != nil {
+			t.Fatalf("Could not create test directory for FileLogger")
+		}
+	}
+	defer os.RemoveAll(TEST_DIR)
+	if fl, err := NewFileLogger(TEST_DIR + "//test"); err == nil {
+		fl.LogAuctionObject(&analytics.AuctionObject{})
+	} else {
+		t.Fatalf("Couldn't initialize file logger: %v", err)
+	}
+}
